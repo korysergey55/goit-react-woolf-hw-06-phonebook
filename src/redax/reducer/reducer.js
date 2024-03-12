f3394bb5aaf823388d166b527f14c275d36a4db7
Serhiy Korytskyy <korysergey55@gmail.com>
2024-03-12 11:35:52 +0200

    root-reducer

--- src/redax/reducer/reducer.js
+++ src/redax/reducer/reducer.js
@@ -1,11 +1,25 @@
 import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
 import { phonebookReducer } from '../slice/slice'
+import storage from 'redux-persist/lib/storage'
 
-const randomReducer = (state = null, _) => {
+const persistConfigContacts = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter']
+}
+
+const persistConfigAuth = {
+  key: 'auth',
+  storage,
+  blacklist: ['contacts'],
+};
+
+const AuthReducer = (state = null, action) => {
   return state
 }
 
 export const rootReducer = combineReducers({
-  contacts: phonebookReducer,
-  random: randomReducer,
+  contacts: persistReducer(persistConfigContacts, phonebookReducer),
+  random: persistReducer(persistConfigAuth, AuthReducer)
 })
--- src/redax/store/store.js
+++ src/redax/store/store.js
@@ -1,20 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { phonebookReducer } from 'redax/slice/slice'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { persistStore } from 'redux-persist'
+import { rootReducer } from 'redax/reducer/reducer'
+// import {  persistReducer } from 'redux-persist'
+// import { phonebookReducer } from 'redax/slice/slice'
+// import storage from 'redux-persist/lib/storage'
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter']
-}
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+//   blacklist: ['filter']
+// }
+// const persistedReducer = persistReducer(persistConfig, phonebookReducer)
 
-const persistedReducer = persistReducer(persistConfig, phonebookReducer)
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistedReducer,
+//   }
+// })
 
 export const store = configureStore({
-  reducer: {
-    contacts: persistedReducer,
-  }
+  reducer: rootReducer
 })
 
 export const persistor = persistStore(store)
